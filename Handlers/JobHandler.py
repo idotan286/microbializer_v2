dfc7d3b080a21198edf5bb63d1a4cacccfe23801
idotan286 <58917533+idotan286@users.noreply.github.com>
2024-07-22 10:44:04 +0300

    Update JobHandler.py - running on SLURM

--- Handlers/JobHandler.py
+++ Handlers/JobHandler.py
@@ -6,9 +6,13 @@ from utils import logger
 from SharedConsts import PATH_TO_OUTPUT_PROCESSOR_SCRIPT, RESULTS_SUMMARY_FILE_NAME, INPUT_CLASSIFIED_FILE_NAME, \
     INPUT_UNCLASSIFIED_FILE_NAME, TEMP_CLASSIFIED_IDS, TEMP_UNCLASSIFIED_IDS, INTERVAL_BETWEEN_LISTENER_SAMPLES, \
     INPUT_CLASSIFIED_FILE_NAME_PAIRED, INPUT_UNCLASSIFIED_FILE_NAME_PAIRED, USER_FILE_NAME_ZIP, USER_FILE_NAME_TAR
-from flask_interface_consts import MICROBIALIZER_PROCESSOR_JOB_QUEUE_NAME, NUBMER_OF_CPUS_MICROBIALIZER_PROCESSOR_JOB, MICROBIALIZER_PROCESSOR_JOB_PREFIX, MICROBIALIZER_PROCESSOR_RESULTS_FILE_NAME, MICROBIALIZER_JOB_TEMPLATE
+from flask_interface_consts import MICROBIALIZER_PROCESSOR_JOB_QUEUE_NAME, NUBMER_OF_CPUS_MICROBIALIZER_PROCESSOR_JOB, \
+    MICROBIALIZER_PROCESSOR_JOB_PREFIX, MICROBIALIZER_PROCESSOR_RESULTS_FILE_NAME, MICROBIALIZER_JOB_TEMPLATE, \
+    ARGS_JSON_PATH_KEY, JOB_PARAMETERS_FILE_NAME
 import glob
+from slurm_example import submit_job
 import datetime
+import json
 
 
 class Handler:
@@ -17,7 +21,7 @@ class Handler:
     """
 
     @staticmethod
-    def submit_micro_job(input_path):
+    def submit_micro_job(input_path, job_arguemnts):
         """
         this function actually preforms the kraken2 search
         :param input_path: path to input query file
@@ -35,26 +39,26 @@ class Handler:
         temp_script_path = input_path_parent / f'temp_micro_search_running_file_{job_unique_id}.sh'
         results_file_path = input_path_parent / MICROBIALIZER_PROCESSOR_RESULTS_FILE_NAME
         job_name = f'{MICROBIALIZER_PROCESSOR_JOB_PREFIX}_{job_unique_id}'
-        temp_script_text = MICROBIALIZER_JOB_TEMPLATE.format(queue_name=MICROBIALIZER_PROCESSOR_JOB_QUEUE_NAME,
-                                          cpu_number=NUBMER_OF_CPUS_MICROBIALIZER_PROCESSOR_JOB, job_name=job_name,
-                                          error_files_path=input_path_parent,
-                                          output_files_path=input_path_parent,
-                                          path_to_folder=input_path_parent,
-                                          path_to_input_validator_script="",
-                                          file_name_1=USER_FILE_NAME_ZIP,
-                                          file_name_2=USER_FILE_NAME_TAR,
-                                          results_file_path=results_file_path,
-                                          sleep_interval=INTERVAL_BETWEEN_LISTENER_SAMPLES,
-                                          mem_req=10)
+        json_parameters_file_path = os.path.join(input_path_parent, JOB_PARAMETERS_FILE_NAME)
+        with open(json_parameters_file_path, 'w') as fp:
+            json.dump(job_arguemnts, fp)
+        command_to_run = MICROBIALIZER_JOB_TEMPLATE.format(
+            sleep_interval=INTERVAL_BETWEEN_LISTENER_SAMPLES,
+            args_json_path_key=ARGS_JSON_PATH_KEY,
+            args_json_path=json_parameters_file_path,
+            results_file_path=results_file_path
+        )
 
         # run the job
-        with open(temp_script_path, 'w+') as fp:
-            fp.write(temp_script_text)
-        logger.info(f'submitting job, temp_script_path = {temp_script_path}:')
-        logger.debug(f'{temp_script_text}')
-        terminal_cmd = f'/opt/pbs/bin/qsub {str(temp_script_path)}'
-        job_run_output = subprocess.run(terminal_cmd, stdout=PIPE, stderr=PIPE, shell=True)
-        # os.remove(temp_script_path)
-        
-        return job_run_output.stdout.decode('utf-8').split('.')[0], results_file_path
+        run_parameters = {
+            "queue": MICROBIALIZER_PROCESSOR_JOB_QUEUE_NAME, 
+            "num_cpus": NUBMER_OF_CPUS_MICROBIALIZER_PROCESSOR_JOB, 
+            "job_name": job_name,
+            "logs_path": input_path_parent,
+            "script_commands": command_to_run,
+            "memory": "10"
+        }
+        run_parameters['logger'] = logger
+        logger.debug(f'{run_parameters}')
+        return submit_job(**run_parameters)
 
