9e1f44c359cde539ee06abb4f5162e9e9c862f92
idotan286 <58917533+idotan286@users.noreply.github.com>
2024-07-28 13:01:05 +0300

    Update results.js - fixing if tree_data

--- app/static/js/results.js
+++ app/static/js/results.js
@@ -118,7 +118,7 @@ const runResultsScript = (histogram_data, orthologous_data, tree_str) => {
     const tree_data = parseNewick(tree_str);
     //console.log(tree_data)
     // only if tree data is have items
-    if (tree_data) {
+    if (tree_data != "") {
         const treeChart = createChart(tree_data)
         let container = document.getElementById("phylo_tree_container")
         container.append(treeChart);
