54a4e37761b0284e5ecdff94acd50d43f7c35380
Yair Shimony <yairshsh@gmail.com>
2024-12-12 14:33:06 +0200

    change email submitted content

--- Job_Manager_API.py
+++ Job_Manager_API.py
@@ -269,8 +269,7 @@ class Job_Manager_API:
             if email_address != None:
                 email_addresses.append(email_address)
 
-            url = consts.MICROBIALIZER_LOCAL_URL if consts.LOCAL else WEBSERVER_ADDRESS
-            self.__build_and_send_mail(process_id, EMAIL_CONSTS.SUBMITTED_TITLE.format(job_name=job_name), EMAIL_CONSTS.SUBMITTED_CONTENT.format(webserver_address=url,process_id=process_id), email_addresses)
+            self.__build_and_send_mail(process_id, EMAIL_CONSTS.SUBMITTED_TITLE.format(job_name=job_name), EMAIL_CONSTS.SUBMITTED_CONTENT.format(process_id=process_id), email_addresses)
             return True
         logger.warning(f'process_id = {process_id}, can\'t add process: is_valid_email = {is_valid_email}')
         return False
