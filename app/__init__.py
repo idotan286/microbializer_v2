641d76e40a20cd9bab91cf6dccfa3a7d4064d3b1
Yair Shimony <yairshsh@gmail.com>
2024-12-17 16:05:22 +0200

    fix http

--- app/__init__.py
+++ app/__init__.py
@@ -41,7 +41,7 @@ class ReverseProxied(object):
     def __call__(self, environ, start_response):
         environ['wsgi.url_scheme'] = 'https'
         # todo remove when server is ready
-        environ['wsgi.url_scheme'] = 'http'
+        # environ['wsgi.url_scheme'] = 'http'
         return self.app(environ, start_response)
 
 app = Flask(__name__)
