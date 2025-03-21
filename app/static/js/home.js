245e3ed72be10aecb41f9f80172147e8a25d2540
shimooper <yairshsh@gmail.com>
2025-03-21 22:12:18 +0200

    try another way to block GA

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -298,4 +298,9 @@ window.onload = function () {
     }
 };
 
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+gtag('consent', 'default', {
+  'analytics_storage': 'denied'
+});
 
